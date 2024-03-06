ed74f0a4a37f6cf2f5eb504efd18beab3ad680ce
陳亮瑜 <liangyu@chenliangyudeMacBook-Pro.local>
2024-03-06 20:02:41 +0800

    ### W3-P1: Use SupaClientGetBlog_xx.jsx (supabase client) to get card_xx data

---
+++ demo/dev/w03-crud_31/src/App_31.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+//import Setup_31 from './tutorials/SupaGetBlog_31'
+import Setup_31 from './tutorials/SupaClientGetBlog_31'
+
+function App_31() {
+
+  return (
+
+    <>
+  {' '}
+
+      <main>
+
+        {' '}
+
+        <div className="title">
+
+          {' '}
+
+          <h1>
+
+            {' '}
+
+            <span>CRUD</span> tutorial{' '}
+
+          </h1>{' '}
+
+        </div>{' '}
+
+        <Setup_31 />{' '}
+
+      </main>{' '}
+
+    </>
+
+  );
+
+}
+
+export default App_31;
---
+++ demo/dev/w03-crud_31/src/tutorials/SupaGetBlog_31.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+let api_url = 'https://szbwskgtksqzkhaokhez.supabase.co/rest/v1/card_31?select=*';
+
+const SupaClientGetBlog_31 = () => {
+  const [name, setName] = useState('Liang Yu');
+  const [id, setId] = useState(208410331);
+  const [blogs, setBlogs] = useState([]);
+  const fetchBlogsFromSupabase = async () => {
+    try {
+      // const response = await fetch(api_url);
+      // const data = await response.json();
+     //let { data, error } = await supabase.from('card_59').select('*');
+
+     const response = await axios.get(api_url, {
+      headers: {
+        apikey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Yndza2d0a3NxemtoYW9raGV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MjMwNjcsImV4cCI6MjAxMTk5OTA2N30.bUnXA78fO4W9Feozn-19qwgpOel9K7ekJnuToGkkWwc',
+        Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Yndza2d0a3NxemtoYW9raGV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MjMwNjcsImV4cCI6MjAxMTk5OTA2N30.bUnXA78fO4W9Feozn-19qwgpOel9K7ekJnuToGkkWwc'
+      }
+     })
+
+      console.log('blogs data', response);
+      //setBlogs(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchBlogsFromSupabase();
+  }, []);
+  return (
+    <section className="blogs">
+      {' '}
+      <div className="section-title">
+        {' '}
+        <h2> Get Blogs Using http </h2>{' '}
+        <h3>
+          {' '}
+          {name}, {id}{' '}
+        </h3>{' '}
+      </div>{' '}
+      <div className="blogs-center">
+        {' '}
+        {blogs.map((item) => {
+          const { id, img, remote_url, title, category, descrip } = item;
+          return (
+            <article key={id} className="blog">
+              {' '}
+              <img src={img} alt="Coffee photo" className="img blog-img" />{' '}
+              <div className="blog-content">
+                {' '}
+                <span>
+                  {' '}
+                  {category} <i className="fa-solid fa-mug-saucer"></i>{' '}
+                </span>{' '}
+                <h3>{title}</h3> <p>{descrip}</p> <a href="#">read more</a>{' '}
+              </div>{' '}
+            </article>
+          );
+        })}{' '}
+      </div>{' '}
+    </section>
+  );
+};
+export default SupaClientGetBlog_31;
