e448a9ad0489c88cfa15f987cdcc8f9c08041944
陳亮瑜 <liangyu@chenliangyudeMacBook-Pro.local>
2024-03-27 21:27:44 +0800

    ### W6-P3: Get all tasks from node server

---
+++ demo/dev/w06-tasknode_31/client_31/src/App_31.jsx
@@ -0,0 +1,22 @@
+import { ToastContainer } from 'react-toastify';
+import { nanoid } from 'nanoid';
+import Form_31 from './Form_31';
+import Items_31 from './Items_31';
+import { useState } from 'react';
+const defaultItems = [
+  { id: nanoid(), title: 'walk the dog', isDone: false },
+  { id: nanoid(), title: 'wash dishes', isDone: false },
+  { id: nanoid(), title: 'drink coffee', isDone: true },
+  { id: nanoid(), title: 'take a nap', isDone: false },
+];
+const App_31 = () => {
+  const [items, setItems] = useState(defaultItems);
+  return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form_31 />
+      <Items_31 items={items} />
+    </section>
+  );
+};
+export default App_31;
---
+++ demo/dev/w06-tasknode_31/client_31/src/utils_31.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const customFetch_31 = axios.create({
+  baseURL: 'http://localhost:5050/api/tasks',
+});
+
+export default customFetch_31;
